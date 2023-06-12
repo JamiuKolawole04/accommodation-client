50f35645db60deb17672996703ffff1afee44079
JamiuKolawole04 <Jakko4real@gmail.com>
2023-06-12 12:28:13 +0100

    chore: changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/api/v1" // THE API (backend) URL
+  baseURL: "https://booking-api.cyclic.app/api/v1", // THE API (backend) URL
+  // baseURL: "http://localhost:8080/api/v1" // THE API (backend) URL
 });
 
 export default instance;
